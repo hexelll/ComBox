b80c474996b4aaa9d46555338816f17ae8fd8d09
hexelll <139350367+hexelll@users.noreply.github.com>
2026-07-19 17:54:29 +0200

    chore(sharpen): forgot to push sharpen

--- pipes/sharpen.lua
+++ pipes/sharpen.lua
@@ -4,7 +4,10 @@ local Color = require "Color"
 
 return function(_,input,alias)
     local sharpen = input[alias] or {}
-    local sharpness = sharpen.strength or 0.5
+    if sharpen.disable then
+        return input
+    end
+    local sharpness = sharpen.strength or 0.75
     input.image = input.image:map(function(self,u,v)
         local x,y = self:uvToXy(u,v)
         local px = self:getPx(u,v)
