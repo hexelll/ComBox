54562bd6a91eb3cb8775c20c9e2e3a1413ad3b04
hexelll <139350367+hexelll@users.noreply.github.com>
2026-07-21 17:40:07 +0200

    fix(import): stop using shell.setDir and shell.resolve

--- import.lua
+++ import.lua
@@ -1,20 +1,17 @@
 local function _import(path,dir)
-    local sdir = shell.dir()
     dir = dir or shell.getRunningProgram()
-    shell.setDir(fs.getDir(dir))
-    local absolutePath = "/"..shell.resolve(path)
+    local absolutePath = fs.combine(fs.getDir(dir),path)
     local fp = fs.open(absolutePath,"r")
     if not fp then error("no such file at "..absolutePath) end
     local code = fp.readAll()
     fp.close()
     local import = _import
-    local fenv = getfenv()
+    local fenv = _ENV
     local env = setmetatable({
         import=function(path,_dir) 
             return _import(path,_dir or absolutePath)
         end
     },{__index=fenv})
-    shell.setDir(sdir)
     return load(code,"@/"..path,nil,env)()
 end
 
